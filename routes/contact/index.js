877520a5edf7c0699e588b44e95af954638651cc
Brett <brett_matsumoto@yahoo.com>
2019-04-29 09:35:08 -1000

    added comments to what was changed

--- routes/contact/index.js
+++ routes/contact/index.js
@@ -4,3 +4,7 @@ const router = express.Router();
 const get = require('./get');
 
 router.get('/contact', get);
+
+module.exports = (req, res) => { //requires a module export
+  res.render('contact');
+};
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
+const app = express(); //moved express() above exphbs;
 const exphbs = require('express-handlebars');
 
-const app = express();
 const PORT = process.env.PORT || 8080;
-const home = require('./router/home');
-const about = require('./router/about');
-const contact = require('./router/contact');
+const home = require('./routes/home'); //misspelled routes with router;
+const about = require('./routes/about'); //misspelled routes with router;
+const contact = require('./routes/contact'); //misspelled routes with router;
 
 app.use(express.static('public'));
-app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
-app.get('/', (req, res) => {
-  res.send('smoke test');
-});
+// app.get('/', (req, res) => {  //don't need this code anymore
+//   res.send('smoke test');
+// });
 
-app.use(home);
-app.use(about);
-app.use(contact);
+app.use('/', home); //'added '/'
+app.use('/', about); //'added '/'
+app.use('/', contact); //'added '/'
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
